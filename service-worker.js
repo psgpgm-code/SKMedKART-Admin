69d41f252f2331f42bbb9892dfe9852f248b5136
psgpgm-code <psgpgm@gmail.com>
2026-08-25 16:57:41 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,16 @@
-const SKMEDKART_FSSAI_LICENCE_NO = "22422039000512";
-const C='skmedkart-admin-vinstallcancel-v3';const A=['./','./index.html','./admin.js','./firebase-config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>r)))});
+const C='skmedkart-v56-complete-corrected-20260825';
+const A=['./','./index.html','./admin.js','./firebase-config.js','./shop-licence-config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ if(e.request.mode==='navigate'){
+   e.respondWith(fetch(e.request).then(r=>r).catch(()=>caches.match('./index.html')));
+   return;
+ }
+ e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone();
+   caches.open(C).then(c=>c.put(e.request,copy));
+   return r;
+ }).catch(()=>caches.match(e.request)));
+});
